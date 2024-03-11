b5d8944aaf56ade0a3a5b6d21b268ddbd15e1398
Om Gate <90276220+OMGATE23@users.noreply.github.com>
2024-03-11 22:08:38 +0530

    feat : message display and css
    Signed-off-by: Om Gate <90276220+OMGATE23@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useEffect, useState } from "react";
+
+import "./App.css";
+import useFirestore from "./hooks/useFirestore";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name, setName] = useState<string>("");
+  const [showChat, setShowChat] = useState(false);
+  const [messageInput, setMessageInput] = useState("");
+  const [response, setResponse] = useState({
+    errorOccured: false,
+  });
+  const [messages, setMessages] = useState({
+    errorOccured: false,
+    messages: [],
+  });
+  const { sendMessage, getMessages } = useFirestore();
+  useEffect(() => {
+    console.log({ messages });
+  }, [messages]);
+  useEffect(() => {
+    console.log({ response });
+  }, [response]);
+  useEffect(() => {
+    getMessages(setMessages);
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!showChat && (
+        <div className="name-modal">
+          <div className="name-form">
+            <label>
+              Enter your name:{" "}
+              <input
+                type="string"
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setName(e.target.value);
+                }}
+                value={name}
+              />
+            </label>
+          </div>
+          <button
+            className="name-form-button"
+            onClick={() => {
+              setShowChat(true);
+            }}
+          >
+            Confirm name
+          </button>
+        </div>
+      )}
+
+      {showChat && (
+        <div className="message-display">
+          <div className="message-container">
+            <ul className="messages">
+              {messages.messages.map((message: any) => (
+                <li
+                  className={`message ${name === message.name && "self"}`}
+                  key={message.id}
+                >
+                  <span className="message-name">{message.name}</span>
+                  <br></br>
+                  <span className="message-content">{message.message}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="message-input-container">
+            <input
+              className="message-input"
+              type="text"
+              value={messageInput}
+              placeholder="Enter Message..."
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setMessageInput(e.target.value);
+              }}
+            />
+
+            <button
+              className="send-btn"
+              disabled={!messageInput}
+              onClick={async () => {
+                await sendMessage(name, messageInput, setResponse);
+                setMessageInput("");
+              }}
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
